f6c74cc37321757df55877dbef3beeb71277b23b
sagrov <howlxfamily@gmail.com>
2022-08-18 21:43:04 +0300

    Monster-inc

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,15 +1,22 @@
 import {Component} from 'react';
 import './search-bos.styles.css'
+import 'font-awesome/css/font-awesome.min.css';
 
 class SearchBox extends Component {
     render() {
         return (
+            <div className="search-box">
+                <button className="btn-search">
+                    <i className="fa fa-search"></i>
+                </button>
             <input
-                className={`search-box ${this.props.className}`}
+                className={`input-search ${this.props.className}`}
                 type='search'
                 placeholder={this.props.placeholder}
                 onChange={this.props.onChangeHandler}
-            />
+                />
+            </div>
+
         )
     }
 }
