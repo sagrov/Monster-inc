7f1906e6b96fd3c7da5360904978e03831b4f8a8
sagrov <howlxfamily@gmail.com>
2022-08-18 19:10:19 +0300

    a

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        monsters: [],
+        searchField: '',
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+        () => {
+        console.log(this.state);
+        }
+        )
+      );
+  }
+
+  render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search for a monster'
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+        }}
+        />
+      {
+        filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+          );
+        }
+        )
+      }
+      </div>
+    );
+  }
+}
+
+export default App;
